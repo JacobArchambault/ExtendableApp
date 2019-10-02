7fd961c47e077a1d8afd68a72763f17659cf82a5
Jacob Archambault <jacob.archambault@waystar.com>
2019-10-02 09:29:11 -0400

    Add project files.

---
+++ CSharpSnapIn/CSharpModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CommonSnappableTypes;
+
+namespace CSharpSnapIn
+{
+    [CompanyInfo(CompanyName = "FooBar", CompanyUrl = "www.FooBar.com")]
+    public class CSharpModule : IAppFunctionality
+    {
+        void IAppFunctionality.Dolt() => Console.WriteLine("You have just used the C# snap-in");
+    }
+}
---
+++ VBSnapIn/VBSnapIn.vb
@@ -0,0 +1,9 @@
+﻿Imports CommonSnappableTypes
+
+<CompanyInfo(CompanyName:="Chucky's Software", CompanyUrl:="www.ChuckySoft.com")>
+Public Class VBSnapIn
+    Implements IAppFunctionality
+    Public Sub Dolt() Implements IAppFunctionality.Dolt
+        Console.WriteLine("You have just used the VB snap in!")
+    End Sub
+End Class
